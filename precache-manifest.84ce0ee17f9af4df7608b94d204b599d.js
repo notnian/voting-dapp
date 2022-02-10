b0f38c4e82fe3dcb942ba2e02924a8edc3e25851
notnian <contact@notnian.dev>
2022-02-10 16:21:52 +0100

    Updates

---
+++ precache-manifest.84ce0ee17f9af4df7608b94d204b599d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b95f171b09e0af41db2695441553c6ce",
+    "url": "/voting-dapp/index.html"
+  },
+  {
+    "revision": "cf8c15829d8e9041e90e",
+    "url": "/voting-dapp/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "76f56fdd3f6f201fb6b5",
+    "url": "/voting-dapp/static/js/2.8387fbd0.chunk.js"
+  },
+  {
+    "revision": "cf8c15829d8e9041e90e",
+    "url": "/voting-dapp/static/js/main.186710ca.chunk.js"
+  },
+  {
+    "revision": "558860fe687b46909716",
+    "url": "/voting-dapp/static/js/runtime-main.27f3d662.js"
+  }
+]);
